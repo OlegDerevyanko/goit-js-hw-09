d60ae840ec3c3a0233de86bf52e0ffa7728cfd93
OlegDerevyanko <106957951+OlegDerevyanko@users.noreply.github.com>
2022-11-07 19:43:55 +0000

    Deploying to gh-pages from @ OlegDerevyanko/goit-js-hw-09@b277dfe904a60b14fdea8203ead5a924fa61a2cc 🚀

---
+++ 01-color-switcher.ca72c39d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)}))}();
+//# sourceMappingURL=01-color-switcher.ca72c39d.js.map
